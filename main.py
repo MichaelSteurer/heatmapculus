96f88d4ea5ca5087c0564d204f4b55b11f236fcb
Michael Steurer <msteurer@iicm.edu>
2014-03-28 15:20:43 +0800

    Added command line parameters

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ Graz University of Technology
 
 #import MySQLdb
 from numpy import vectorize
+import argparse
 import sys
 import string
+import os
 import pprint
 import csv
 import numpy as np
@@ -35,8 +37,10 @@ if len(logging.getLogger().handlers) > 1:
 
 
 def parse_float(num):
-    try: return float(num.replace(",", "."))
-    except ValueError: return "Nan"
+    try:
+      return float(num.replace(",", "."))
+    except ValueError:
+      return "Nan"
 
 
 def parse_file(csv_file):
@@ -60,6 +64,7 @@ def transform_array_to_heatmap(coordinates, resize_factor=1):
 
   x_list = [element[1] for element in coordinates]
   z_list = [element[3] for element in coordinates]
+
 
   min_x, max_x = int(min(x_list)), int(max(x_list))
   min_z, max_z = int(min(z_list)), int(max(z_list))
@@ -89,21 +94,26 @@ def transform_array_to_heatmap(coordinates, resize_factor=1):
   return heatmap_array
 
 
-def main(csv_file):
-  img = imread("data/terminal_mapview.png")
+def main(csv_file, background_image, heatmap_scale, offset_x, offset_y):
+  img = imread(background_image)
   image_height = img.shape[0]
   image_width = img.shape[1]
 
-  image_height_resized = int(image_height)
-  image_width_resized = int(image_width)
-
-  coordinates = parse_file(csv_file)
+  coordinates = list()
+  if os.path.isdir(csv_file):
+    for filename in os.listdir(csv_file):
+      try:
+        coordinates += parse_file(csv_file + "/" + filename)
+      except:
+        logging.error("Could not parse file %s" % filename)
+  else:
+    coordinates += parse_file(csv_file)
 
   heatmap_array = transform_array_to_heatmap(coordinates, resize_factor=1)
 
   ##Filter Array
   def array_transformed(x):
-    return x if x!= 0 else -np.inf
+    return np.log(x) if x!= 0 else -np.inf
   func = vectorize(array_transformed)
   heatmap_array = func(heatmap_array)
 
@@ -111,10 +121,6 @@ def main(csv_file):
 
   ##Show Heatmap
   plt.figure(frameon=False, figsize=(15,12))
-
-  heatmap_scale = 6.2
-  offset_x = -22
-  offset_y = 110
 
   #determine offset according to the first and min/max coordinates
   min_x = min([element[1] for element in coordinates])
@@ -133,16 +139,31 @@ def main(csv_file):
   heatmap_size_y = heatmap_array.shape[0] * heatmap_scale
 
   imshow(heatmap_array, alpha=1, extent=(delta_x, delta_x + heatmap_size_x, delta_y, delta_y + heatmap_size_y))
-  plt.imshow(img, zorder=-1, extent=(0, image_width_resized, image_height_resized, 0))
+  plt.imshow(img, zorder=-1, extent=(0, image_width, image_height, 0))
 
   plt.show()
 
+
+#  heatmap_scale = 6.2
+#  offset_x = -20
+#  offset_y = 180
+
 if __name__ == "__main__":
-  if len(sys.argv) == 2:
-    csv_file = sys.argv[1]
-    main(csv_file)
-  else:
-    print "Usage wrong"
-    sys.exit(-2)
+  parser = argparse.ArgumentParser(
+           description='',
+           add_help=False)
+  parser.add_argument('-s', '--source', required=True, help="")
+  parser.add_argument('-m', '--magnify', required=True, type=float, help="")
+  parser.add_argument('-x', '--offset_x', required=True, type=int, help="")
+  parser.add_argument('-y', '--offset_y', required=True, type=int, help="")
+  parser.add_argument('-i', '--background_image', required=True, help="")
+  parser.add_argument('--help', action='help')
+  cmd = parser.parse_args()
+
+#  if not os.path.isdir(cmdValues):
+#    print "The provided parameter is not a directory"
+#    sys.exit(-2)
+
+  main(cmd.source, cmd.background_image, cmd.magnify, cmd.offset_x, cmd.offset_y)
 
   sys.exit(0)
